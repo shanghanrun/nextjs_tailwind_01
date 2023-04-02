a6454104feaea592cc1bab5360adaaa6fb59a210
shanghanrun <120538041+shanghanrun@users.noreply.github.com>
2023-04-02 22:50:01 +0900

    홈 사진추가

--- components/herostyle.js
+++ components/herostyle.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from 'next/image';
+import next from './next.jpeg';
 
 export default function HeroStyle(){
   return(
@@ -6,19 +8,19 @@ export default function HeroStyle(){
       <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                <br className="hidden lg:inline-block"/>readymade gluten
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">반갑습니다. Choco's Dev입니다.
+                <br className="hidden lg:inline-block"/>NEXT TAIL
               </h1>
-              <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+              <p className="mb-8 leading-relaxed">이번 프로젝트에서는 넥스트제이에스와 테일윈드를 통해서, 빠른 시간에 화면을 구성하고, 더 나아가 nextjs의 기능들을 더 살펴볼 예정입니다.</p>
               <div className="flex justify-center">
                 <Link href="/project">
                   <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트로 가기</button> 
                 </Link>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+                {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> */}
               </div>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+              <Image src={next}></Image>
             </div>
           </div>
       </section>
